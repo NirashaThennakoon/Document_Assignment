2b604fe0bd81307f9a6a08b60e83b78a986a4c1f
Nirasha Thennakoon <nmalinthi@gmail.com>
2019-04-16 08:59:44 +0530

    Add files via upload

---
+++ src/main/java/com/nirasha/smsweb/model/Page.java
@@ -0,0 +1,64 @@
+package com.nirasha.smsweb.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Page {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer pid;
+	
+	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "cid")
+	private Card card;
+
+	@OneToMany(mappedBy = "page",cascade = CascadeType.ALL)
+	List<Section> sections;
+
+	
+	public List<Section> getSections() {
+		return sections;
+	}
+
+	public void setSections(List<Section> sections) {
+		this.sections = sections;
+	}
+
+	public Integer getPid() {
+		return pid;
+	}
+
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+
+	public Card getCard() {
+		return card;
+	}
+
+	public void setCard(Card card) {
+		this.card = card;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/nirasha/smsweb/repository/DocumentRepository.java
@@ -0,0 +1,9 @@
+package com.nirasha.smsweb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nirasha.smsweb.model.Document;
+
+public interface DocumentRepository extends JpaRepository<Document, Integer>{
+
+}
